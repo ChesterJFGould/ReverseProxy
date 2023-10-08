f8110adf4ec4d4a54a58af423bfe4edba12b823e
Techser <chestergould@protonmail.com>
2023-10-08 16:50:21 -0700

    updated config

--- reverseProxy.go
+++ reverseProxy.go
@@ -1,47 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
+        "fmt"
+        "net/http"
+        "net/http/httputil"
+        "net/url"
+        "os"
 )
 
 const (
-	certPath = "/etc/letsencrypt/live/chestergould.xyz/fullchain.pem"
-	keyPath = "/etc/letsencrypt/live/chestergould.xyz/privkey.pem"
+        certPath = "/etc/letsencrypt/live/chestergould.ca/fullchain.pem"
+        keyPath = "/etc/letsencrypt/live/chestergould.ca/privkey.pem"
 )
 
 func printErrorAndExit(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-		os.Exit(1)
-	}
+        if err != nil {
+                fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+                os.Exit(1)
+        }
 }
 
 func redirectHTTPS(w http.ResponseWriter, r *http.Request) {
-	var httpsUrl url.URL
+        var httpsUrl url.URL
 
-	httpsUrl = *r.URL
-	httpsUrl.Host = r.Host
-	httpsUrl.Scheme = "https"
+        httpsUrl = *r.URL
+        httpsUrl.Host = r.Host
+        httpsUrl.Scheme = "https"
 
-	http.Redirect(w, r, httpsUrl.String(), http.StatusPermanentRedirect)
+        http.Redirect(w, r, httpsUrl.String(), http.StatusPermanentRedirect)
 }
 
 func main () {
-	go http.ListenAndServe(":80", http.HandlerFunc(redirectHTTPS))
+        go http.ListenAndServe(":80", http.HandlerFunc(redirectHTTPS))
 
-	websiteUrl, err := url.Parse("http://localhost:3141")
-	printErrorAndExit(err)
+        websiteUrl, err := url.Parse("http://localhost:3141")
+        printErrorAndExit(err)
+        pleromaUrl, err := url.Parse("http://localhost:4000")
+        printErrorAndExit(err)
 
-	searchUrl, err := url.Parse("http://localhost:2718")
-	printErrorAndExit(err)
+        /*
+        searchUrl, err := url.Parse("http://localhost:2718")
+        printErrorAndExit(err)
+        */
 
-	http.Handle("chestergould.xyz/", httputil.NewSingleHostReverseProxy(websiteUrl))
-	http.Handle("www.chestergould.xyz/", httputil.NewSingleHostReverseProxy(websiteUrl))
-	http.Handle("search.chestergould.xyz/", httputil.NewSingleHostReverseProxy(searchUrl))
-	err = http.ListenAndServeTLS(":443", certPath, keyPath, nil)
-	printErrorAndExit(err)
+        http.Handle("chestergould.ca/", httputil.NewSingleHostReverseProxy(websiteUrl))
+        http.Handle("www.chestergould.ca/", httputil.NewSingleHostReverseProxy(websiteUrl))
+        http.Handle("pleroma.chestergould.ca/", httputil.NewSingleHostReverseProxy(pleromaUrl))
+        http.Handle("minecraft.chestergould.ca")
+        /*
+        http.Handle("search.chestergould.xyz/", httputil.NewSingleHostReverseProxy(searchUrl))
+        */
+        err = http.ListenAndServeTLS(":443", certPath, keyPath, nil)
+        printErrorAndExit(err)
 }
